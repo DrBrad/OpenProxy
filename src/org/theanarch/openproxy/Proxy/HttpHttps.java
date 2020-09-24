7dab48b71348ec3500b75278a45a4d87bd32e3d6
Brad Eagle <drbradtech@gmail.com>
2020-09-24 14:55:39 -0600

    Update HttpHttps.java

--- src/org/theanarch/openproxy/Proxy/HttpHttps.java
+++ src/org/theanarch/openproxy/Proxy/HttpHttps.java
@@ -1,8 +1,6 @@
 package org.theanarch.openproxy.Proxy;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.URL;
@@ -35,39 +33,24 @@ public class HttpHttps {
     public void connect(Matcher matcher)throws Exception {
         InetAddress address = InetAddress.getByName(new URL("https://"+matcher.group(2)).getHost());
         tunnel.server = new Socket(address, Integer.parseInt(matcher.group(4)));
+        tunnel.serverIn = tunnel.server.getInputStream();
+        tunnel.serverOut = tunnel.server.getOutputStream();
 
         tunnel.clientOut.write(("HTTP/"+matcher.group(5)+" 200 Connection established\r\n").getBytes());
-        tunnel.clientOut.write(("Protocol-agent: UNet/0.1\r\n").getBytes());
+        tunnel.clientOut.write(("Protocol-agent: OpenProxy/0.1\r\n").getBytes());
         tunnel.clientOut.write(("\r\n").getBytes());
         tunnel.clientOut.flush();
-
-        new Thread(new Runnable(){
-            @Override
-            public void run(){
-                forwardData(tunnel.server, tunnel.socket);
-            }
-        }).start();
-
-        forwardData(tunnel.socket, tunnel.server);
     }
 
     public void get(URL url, String response)throws Exception {
         InetAddress address = InetAddress.getByName(url.getHost());
         tunnel.server = new Socket(address, 80);
+        tunnel.serverIn = tunnel.server.getInputStream();
+        tunnel.serverOut = tunnel.server.getOutputStream();
 
         byte[] buffer = response.getBytes();
-        OutputStream out = tunnel.server.getOutputStream();
-        out.write(buffer, 0, buffer.length);
-        out.flush();
-
-        new Thread(new Runnable(){
-            @Override
-            public void run(){
-                forwardData(tunnel.socket, tunnel.server);
-            }
-        }).start();
-
-        forwardData(tunnel.server, tunnel.socket);
+        tunnel.serverOut.write(buffer, 0, buffer.length);
+        tunnel.serverOut.flush();
     }
 
     public String readLine()throws IOException {
@@ -86,37 +69,4 @@ public class HttpHttps {
 
         return builder;
     }
-
-    public static void forwardData(Socket inputSocket, Socket outputSocket){
-        try{
-            InputStream inputStream = inputSocket.getInputStream();
-            try{
-                OutputStream outputStream = outputSocket.getOutputStream();
-                try{
-                    byte[] buffer = new byte[4096];//4096
-                    int read;
-                    do{
-                        read = inputStream.read(buffer);
-                        if(read > 0){
-                            outputStream.write(buffer, 0, read);
-                            if(inputStream.available() < 1){
-                                outputStream.flush();
-                            }
-                        }
-                    }while(read >= 0);
-                }catch(Exception e){
-                }finally{
-                    if(!outputSocket.isOutputShutdown()){
-                        outputSocket.shutdownOutput();
-                    }
-                }
-            }finally{
-                if(!inputSocket.isInputShutdown()){
-                    inputSocket.shutdownInput();
-                }
-            }
-        }catch(IOException e){
-        }
-    }
-
 }
