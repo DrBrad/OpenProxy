1f1313cd47791c71c5196217536c4ad473303703
Brad Eagle <drbradtech@gmail.com>
2020-09-24 14:56:51 -0600

    Update Main.java

--- src/org/theanarch/openproxy/Main.java
+++ src/org/theanarch/openproxy/Main.java
@@ -6,5 +6,18 @@ public class Main {
 
     public static void main(String[] args){
         new Proxy();
+
+        UPnP uPnP = new UPnP();
+        if(!uPnP.isMappedTCP(8080)){
+            uPnP.openPortTCP(8080);
+            System.out.println("Port 8080 mapped using UPnP");
+        }
+
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable(){
+            @Override
+            public void run(){
+                uPnP.closePortTCP(8080);
+            }
+        }));
     }
 }
